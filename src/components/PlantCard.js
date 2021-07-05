3f58e947bdc8bbdffd81e41b0a006c25bd09f469
Priscila Veras <vrspriscila@gmail.com>
2021-07-05 22:30:59 +0100

    Criando cards plantas

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,17 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-function PlantCard() {
+function PlantCard(props) {
   return (
     <div>
-      <div className="card" style={{ width: "13rem", height: "15rem" }}>
-        <img src="..." className="card-img-top" alt="..." />
+      <div className="card" style={{ width: "200px", height: "200px" }}>
+        <img src={props.imagem} className="card-img-top" alt="..." />
         <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
+          <h4 className="card-title">{props.nomePlanta}</h4>
+          <h5 className="card-title">{props.nomeCientifico}</h5>
+          <p className="card-text">{props.descricao}</p>
           <Link to="#" className="btn btn-primary"></Link>
         </div>
       </div>
